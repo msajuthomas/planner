a7b5c680b1316c43c244625506740682e9a8835e
Michelle S Thomas <michellesajuthomas@gmail.com>
2026-02-10 16:01:04 +1100

    Added exit message and run loop

--- project-backend/main.py
+++ project-backend/main.py
@@ -1,1 +1,11 @@
+import helper as func
 print("Welcome to your personal planner!")
+name = input("What is your name? ")
+print("Hello", end=" ")
+print(name + "!")
+
+func.running()
+
+print("All ready...")
+print("Goodbye")
+
